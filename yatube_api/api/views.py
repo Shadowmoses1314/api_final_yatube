b70d417bbc780b2286bfb83bc40ae0effa9d8feb
Григорий Шабалкин <ShadowMoses1314@mail.ru>
2022-12-27 23:49:16 +0600

    First

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import get_object_or_404
+from posts.models import Group, Post
 from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 
-from posts.models import Group, Post
-
 from api.permissions import IsAuthorOrReadOnly
-from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
-                          PostSerializer)
+
+from api.serializers import (
+    CommentSerializer,
+    FollowSerializer,
+    GroupSerializer,
+    PostSerializer)
 
 
 class CreateListViewSet(mixins.CreateModelMixin,
