f63490730d7f3116ace543c84726c80c6613f064
Григорий Шабалкин <ShadowMoses1314@mail.ru>
2022-12-27 15:22:36 +0600

    First

---
+++ yatube_api/api/permissions.py
@@ -0,0 +1,13 @@
+
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Пользовательское разрешение,
+    позволяющее только владельцам объекта редактировать его.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
